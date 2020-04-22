3687142060d2058ca5b6bd487af098fa9f8546ae
sori883 <service@dlme.biz>
2020-04-22 22:28:30 +0900

    indexの呼び出し元controller作成 とりあえずダミーデータ

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    public function index()
+    {
+        // ダミーデータ
+        $articles = [
+            (object) [ // (object)は型キャスト。配列をobjectに変更している（DBから読み込んだデータはobjectになるのでそれに合わせるため）
+                'id' => 1,
+                'title' => 'タイトル1',
+                'body' => '本文1',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 1,
+                    'name' => 'ユーザー名1',
+                ],
+            ],
+            (object) [
+                'id' => 2,
+                'title' => 'タイトル2',
+                'body' => '本文2',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 2,
+                    'name' => 'ユーザー名2',
+                ],
+            ],
+            (object) [
+                'id' => 3,
+                'title' => 'タイトル3',
+                'body' => '本文3',
+                'created_at' => now(),
+                'user' => (object) [
+                    'id' => 3,
+                    'name' => 'ユーザー名3',
+                ],
+            ],
+        ];
+ 
+        // laravel-sns\laravel\resources\views\articles\indexを参照する
+        return view('articles.index', ['articles' => $articles]);
+    }
+}
