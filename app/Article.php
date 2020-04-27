a21e5e08488df3d97ff9921d67d556e785ddb43b
sori883 <service@dlme.biz>
2020-04-27 21:14:45 +0900

    usersテーブルとのリレーション追加

---
+++ app/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    public function user(): BelongsTo // BelongsToは戻り値の型
+    {
+        // belongsToメソッドは、関係するモデルとのリレーションを返す。
+        // belongsToメソッドの場合は、BelongsToクラス
+        return $this->belongsTo('App\User');
+
+        // 1対1のときは、return $this->hasOne('App\User');
+        // 1対多のときは、return $this->hasMany('App\User');
+        // 多対多ときは、return $this->belongsToMany('App\User');
+    }
+}
